f1fa58ff49ecf3035bb800827b7666d9fca2f7d3
MohamedMF2 <furious_fouad@yahoo.com>
2022-05-13 20:44:05 +0200

    added new list of grades and their average result

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace gradebook
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            // var numbers = new[] { 10.5, 15.4, 22.5, 20 };
+            List<double> grades = new List<double>() { 10.5, 15.4, 22.5, 20 };
+            // var gradesCount = grades.Count;
+            // Console.WriteLine($"gradesCount = {gradesCount}");
+
+            var result = 0.0;
+
+            foreach (var grade in grades)
+            {
+                result += grade;
+
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"average Grade = {result:N3}");
+
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]} !");
